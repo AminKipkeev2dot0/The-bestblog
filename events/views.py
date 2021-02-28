7527d7c661a7862e548d596fa71ff90bf4bc7d45
AminKipkeev2dot0 <aminkipkeev12@gmail.com>
2021-03-01 02:38:47 +0300

    worked on bootstrap and made cards of blog

--- events/views.py
+++ events/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Event
 
 # Create your views here.
+def main (request):
+    event = Event.objects
+    return render (request, 'events/main.html', {'events':event})
